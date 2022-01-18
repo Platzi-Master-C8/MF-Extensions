a513d709891f5805ebf3e34902fdc606f0aafdcf
Felipe Duitama <felipedc09@gmail.com>
2022-01-17 22:35:52 -0500

    feat: add to show vacancies and vacancy detail

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
-
 import Theme from 'Pages/Theme';
-import Extensions from 'Pages/Extensions';
 import NotFound from 'Pages/NotFound';
+import Vacancies from '../pages/vacancies';
+import Vacancy from '../pages/vacancies/[vacancyId]';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Theme />} />
-            <Route path="/extensions" element={<Extensions />} />
+            <Route path="/vacancies" element={<Vacancies />} />
+            <Route path="/vacancies/:vacancyId" element={<Vacancy />} />
+            <Route path="*" element={<NotFound />} />
         </Switch>
     </BrowserRouter>
 );
