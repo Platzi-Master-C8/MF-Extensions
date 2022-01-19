4674fbdda53da3beb6360398adc11eca590e3736
Felipe Duitama <felipedc09@gmail.com>
2022-01-19 11:42:42 -0500

    feat: add load vacancy and detail vacancy view

--- src/pages/vacancies/[vacancyId]/vacancy.styles.js
+++ src/pages/vacancies/[vacancyId]/vacancy.styles.js
@@ -1,10 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
-  root: {
+ const useStyles = makeStyles((theme) => ({
+  form: {
+    padding: '10px'
+  },
+  main: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap:'20px'
+  },
+  section: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: 200,
     },
   },
+  contact: {
+    border: '1px solid gray',
+    padding: '10px',
+    borderRadius: '5px'
+  },
+  score: {
+    border: '1px solid purple',
+    padding: '10px',
+    margin: '20px 0px',
+    borderRadius: '5px'
+  },
 }));
+export default useStyles
