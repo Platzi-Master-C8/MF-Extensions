720211139810ef5da6abc62e6fb866bbe59b32e0
Felipe Duitama <felipedc09@gmail.com>
2022-02-01 23:39:09 -0500

    feat: add auth support in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import Routes from 'Routes';
-import { THEME } from 'Constants/theme.constant';
-import { ThemeProvider } from '@mui/material';
-import 'Styles/style.scss';
+import { useRoutes } from 'react-router-dom';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from './theme/ThemeProvider';
+import routes from './routes';
 
-const App = () => (
-    <React.StrictMode>
-        <ThemeProvider theme={THEME}>
-            <Routes />
+const App = () => {
+    const content = useRoutes(routes);
+    const loginURL = `https://platzimaster.us.auth0.com/authorize?response_type=${process.env.TOKEN}&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&audience=${process.env.AUDIENCE}`;
+    
+    const getToken = () => {
+        if (window.location.hash) {
+            const token = window.location.hash.split('=')[1].split('&')[0];
+            localStorage.setItem('token', token);
+        }
+    };
+
+    const verifyIfToken = () => {
+        if (!localStorage.getItem('token')) {
+            window.location.href = loginURL;
+        }
+        if (window.location.hash.includes('access')) {
+            window.location.href = '/vacancies';
+        }
+    };
+
+    getToken();
+    verifyIfToken();
+
+    return (
+        <ThemeProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <CssBaseline />
+                {content}
+            </LocalizationProvider>
         </ThemeProvider>
-    </React.StrictMode>
-);
-
+    );
+};
 export default App;
