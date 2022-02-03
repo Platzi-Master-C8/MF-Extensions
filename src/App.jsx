9c18dd2ffdae641088be5a1d73c84d8ff37bfde8
Felipe Duitama <felipedc09@gmail.com>
2022-02-02 22:47:50 -0500

    fix: add log

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRoutes } from 'react-router-dom';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -9,25 +9,29 @@ import routes from './routes';
 const App = () => {
     const content = useRoutes(routes);
     const loginURL = `https://platzimaster.us.auth0.com/authorize?response_type=${process.env.TOKEN}&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&audience=${process.env.AUDIENCE}`;
-    
-    const getToken = () => {
-        if (window.location.hash) {
-            const token = window.location.hash.split('=')[1].split('&')[0];
-            localStorage.setItem('token', token);
-        }
-    };
 
-    const verifyIfToken = () => {
-        if (!localStorage.getItem('token')) {
-            window.location.href = loginURL;
-        }
-        if (window.location.hash.includes('access')) {
-            window.location.href = '/vacancies';
-        }
-    };
+    console.log(loginURL);
 
-    getToken();
-    verifyIfToken();
+    useEffect(() => {
+        const getToken = () => {
+            if (window.location.hash) {
+                const token = window.location.hash.split('=')[1].split('&')[0];
+                localStorage.setItem('token', token);
+            }
+        };
+
+        const verifyIfToken = () => {
+            if (!localStorage.getItem('token')) {
+                window.location.href = loginURL;
+            }
+            if (window.location.hash.includes('access')) {
+                window.location.href = '/vacancies';
+            }
+        };
+
+        getToken();
+        verifyIfToken();
+    }, []);
 
     return (
         <ThemeProvider>
