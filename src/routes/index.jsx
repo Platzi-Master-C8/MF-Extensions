61431a7c754f7325eea24d2378e0c37b87cadeb5
Bulmaro Arellano <tomaro@outlook.es>
2022-02-09 02:18:01 -0600

    feat: correct errors try onne

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/jsx-props-no-spreading */
@@ -5,9 +6,9 @@ import React, { Suspense, lazy } from 'react';
 
 import { Navigate } from 'react-router-dom';
 
-import SuspenseLoader from '../components/atoms/SuspenseLoader';
-import SidebarLayout from '../layouts/SidebarLayout';
-import BaseLayout from '../layouts/BaseLayout';
+import BaseLayout from 'src/layouts/BaseLayout';
+import SuspenseLoader from "Components/atoms/SuspenseLoader";
+import SidebarLayout from "../layouts/SidebarLayout";
 
 const Loader = (Component) => (props) =>
     (
@@ -19,8 +20,6 @@ const Loader = (Component) => (props) =>
 
 // const Vacancy = Loader(lazy(() =>
 //     import ('../pages/vacancies/[vacancyId]')));
-// const Vacancy = Loader(lazy(() =>
-//     import ('../pages/vacancies/[vacancyId]')));
 const Vacancies = Loader(lazy(() => import('../pages/Vacancies')));
 const Status404 = Loader(lazy(() => import('../pages/Status/Status404')));
 const Status500 = Loader(lazy(() => import('../pages/Status/Status500')));
@@ -28,7 +27,8 @@ const Dashboard = Loader(lazy(() => import('../dashboard')));
 const StatusComingSoon = Loader(lazy(() => import('../pages/Status/ComingSoon')));
 const StatusMaintenance = Loader(lazy(() => import('../pages/Status/Maintenance')));
 const NotFound = Loader(lazy(() => import('../pages/Status/Status404')));
-
+const SingleVacancy = Loader(
+    lazy(() => import("../pages/Vacancies/single")));
 const routes = [
     {
         path: '/',
@@ -38,10 +38,32 @@ const routes = [
                 path: 'dashboard',
                 element: <Dashboard />,
             },
-            {
-                path: 'vacancies',
-                element: <Vacancies />,
-            },
+ {
+    path: 'vacancies',
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="list" replace />
+      },
+      {
+        path: 'list',
+        element: <Vacancies />
+      },
+      {
+        path: 'single',
+        children: [
+          {
+            path: '/',
+            element: <Navigate to="1" replace />
+          },
+          {
+            path: ':vacancyId',
+            element: <SingleVacancy />
+          }
+        ]
+      }
+    ]
+  }
         ],
     },
 ];
